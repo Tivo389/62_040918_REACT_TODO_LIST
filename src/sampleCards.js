c8ce71b6ad90a98e57a8081a5b13f447423d51a2
Shun Miyaki <shunmiyaki@hotmail.com>
2018-04-20 15:28:13 +0900

    Made adjustments to update method - Adjusted variable names for easier understanding. - Trying to see if one update method can deal with all of it.

--- src/sampleCards.js
+++ src/sampleCards.js
@@ -1,35 +1,35 @@
 const taskCards = {
 
   card1: {
-    name: "Shopping List",
-    color: "#fffee8",
-    tasks: {
+    cardName: "Shopping List",
+    cardColor: "#fffee8",
+    cardTasks: {
       task1: {
-        name: "Buy Milk",
-        done: 'true'
+        taskName: "Buy Milk",
+        taskDone: 'true'
       },
       task2: {
-        name: "Buy Flour",
-        done: 'false'
+        taskName: "Buy Flour",
+        taskDone: 'false'
       },
       task3: {
-        name: "Buy Eggs",
-        done: 'true'
+        taskName: "Buy Eggs",
+        taskDone: 'true'
       }
     }
   },
 
   card2: {
-    name: "Read Books",
-    color: "#e6fff5",
-    tasks: {
+    cardName: "Read Books",
+    cardColor: "#e6fff5",
+    cardTasks: {
       task1: {
-        name: "Harry Potter - Azkaban Prison Break - ",
-        done: 'false'
+        taskName: "Harry Potter - Azkaban Prison Break - ",
+        taskDone: 'false'
       },
       task2: {
-        name: "Sunset - The end of the rising sun - ",
-        done: 'true'
+        taskName: "Sunset - The end of the rising sun - ",
+        taskDone: 'true'
       }
     }
   }
