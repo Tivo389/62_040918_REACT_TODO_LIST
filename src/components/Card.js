a37ae0228041a053ab77f617e51fba7e2ae04567
Shun Miyaki <shunmiyaki@hotmail.com>
2018-05-03 22:43:25 +0900

    Caret Restored. Partially into changing color

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import CardTask from './CardTask';
 import CardName from './CardName';
+import ColorPicker from './ColorPicker';
 
 class Card extends React.Component {
 
@@ -17,8 +18,6 @@ class Card extends React.Component {
     const updatedCard = {
       ...cardDetails
     };
-    // CONTINUE FROM HERE : Just finished updating the states correctly. CaretPos is now saved in state correctly.
-    // NEXT: Based on the last Caret Position use it in component did update...?
     updateLastState(cardIndex, property, caret);
     updateCard(cardIndex, updatedCard);
   };
@@ -45,37 +44,12 @@ class Card extends React.Component {
     return caretOffset;
   };
 
-  // Original code from online, kept for reference.
-  // getCaretPos = (e) => {
-  //   const element = e.currentTarget;  // Define the current element
-  //   let caretOffset = 0;  // Set default caret position
-  //   const doc = element.ownerDocument || element.document;
-  //   const win = doc.defaultView || doc.parentWindow;
-  //   let sel;
-  //   if (typeof win.getSelection !== "undefined") {
-  //     sel = win.getSelection(); // Returns a Selection object representing the range of text selected by the user OR the current position of the caret.
-  //     if (sel.rangeCount > 0) {  // The rangeCount is in most cases 1.
-  //       const range = win.getSelection().getRangeAt(0);  // Returns a range object representing the current selected range of text.
-  //       const preCaretRange = range.cloneRange();  // Creates a duplicate of the above range object.
-  //       preCaretRange.selectNodeContents(element);  // "Set [range object (preCaretRange.)] to [contain (selectNodeContents.)] this [element]"
-  //       preCaretRange.setEnd(range.endContainer, range.endOffset);  // "Based on this [range (preCaretRange)], [.setEnd] to, ([target object], [end range location])"
-  //     }
-  //   } else if ( (sel = doc.selection) && sel.type !== "Control") {
-  //     const textRange = sel.createRange();
-  //     const preCaretTextRange = doc.body.createTextRange();
-  //     preCaretTextRange.moveToElementText(element);
-  //     preCaretTextRange.setEndPoint("EndToEnd", textRange);
-  //     caretOffset = preCaretTextRange.text.length;
-  //   }
-  //   return caretOffset;
-  // };
-
   componentDidUpdate() {
-    const cardName = document.querySelector(`[data-name=${this.props.lastCard}]`);
-    const cardProperty = cardName.querySelector(`[data-name=${this.props.lastProperty}]`);
+    const cardName = document.querySelector(`[data-name=${this.props.lastCard}]`) || '';
+    const cardProperty = cardName.querySelector(`[data-name=${this.props.lastProperty}]`) || '';
     const textNode = cardProperty.firstChild;
     if(textNode != null) {
-      const caret = textNode.length;
+      const caret = this.props.lastCaretPosition;
       const range = document.createRange();
       range.setStart(textNode, caret);
       range.setEnd(textNode, caret);
@@ -90,7 +64,6 @@ class Card extends React.Component {
     const divStyle = { backgroundColor: cardDetails.cardColor };
     return(
       <div className="cardContainer" data-name={cardIndex} style={divStyle}>
-
         <CardName
           name="cardName"
           cardDetails={cardDetails}
@@ -112,7 +85,7 @@ class Card extends React.Component {
           ))}
         </ul>
         <div className="cardToolBox">
-          <i className="fas fa-paint-brush"></i>
+          <ColorPicker />
           <i className="fas fa-trash-alt"></i>
         </div>
       </div>
