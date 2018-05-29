8c3b28a36d410b5da568d6459601d435a765f8b2
Shun Miyaki <shunmiyaki@hotmail.com>
2018-05-29 16:15:17 +0900

    pre-expriment of onSubmit

--- src/components/StartPage.js
+++ src/components/StartPage.js
@@ -11,15 +11,36 @@ class StartPage extends React.Component {
 
   goToNote = (e) => {
     e.preventDefault();
-    const nodeID = this.myInput.current.value;
-    this.props.history.push(`/note/${nodeID}`);
+    this.startTakeNote(e);
+    // const nodeID = this.myInput.current.value;
+    // this.props.history.push(`/note/${nodeID}`);
+  };
+
+  startTakeNote = (e) => {
+    const ect = e.currentTarget;
+    const btnSubmit = document.querySelector("button[type='submit']");
+    const faCheckSq = btnSubmit.querySelector('.fa-check-square');
+    // ect.classList.add('activated');
+    // btnSubmit.classList.add('activated');
+    faCheckSq.classList.toggle('activated');
   };
 
   render() {
     return(
       <form action="" className="nodeID" onSubmit={this.goToNote}>
-        <input type="text" placeholder="Note ID Number" defaultValue={Date.now()} ref={this.myInput} required />
-        <button type="submit">Make A List</button>
+        <input
+          type="text"
+          placeholder="Note ID Number"
+          defaultValue={Date.now()} ref={this.myInput}
+          className="hidden"
+          required />
+        <button type="submit">
+          <span className="fa-layers fa-fw">
+            <i className="far fa-square"></i>
+            <i className="far fa-check-square"></i>
+          </span>
+          Take Note
+        </button>
       </form>
     )
   }
