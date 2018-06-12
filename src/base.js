333bdd8b575db7e1efd2240b61cba12a15dabcb9
Shun Miyaki <shunmiyaki@hotmail.com>
2018-06-12 17:58:33 +0900

    Config updated

--- src/base.js
+++ src/base.js
@@ -1,15 +1,8 @@
 import Rebase from 're-base';
 import firebase from 'firebase';
+import config from './config/config.js';
 
-const firebaseApp = firebase.initializeApp({
-  apiKey: "AIzaSyBQ0Ra3esqtouShN30pcFsbGy5GjMoKTqQ",
-  authDomain: "react-todo-list-9b926.firebaseapp.com",
-  databaseURL: "https://react-todo-list-9b926.firebaseio.com",
-  projectId: "react-todo-list-9b926",
-  storageBucket: "react-todo-list-9b926.appspot.com",
-  messagingSenderId: "730427638952"
-});
-
+const firebaseApp = firebase.initializeApp(config);
 const base = Rebase.createClass(firebaseApp.database());
 
 export { firebaseApp };
