7e6b5a8f74fe6acc315e7a60f3d91e936939fb8d
Shun Miyaki <shunmiyaki@hotmail.com>
2018-06-12 18:09:38 +0900

    BaseJS removed

--- src/base.js
+++
@@ -1,9 +0,0 @@
-import Rebase from 're-base';
-import firebase from 'firebase';
-import config from './config/config.js';
-
-const firebaseApp = firebase.initializeApp(config);
-const base = Rebase.createClass(firebaseApp.database());
-
-export { firebaseApp };
-export default base;
